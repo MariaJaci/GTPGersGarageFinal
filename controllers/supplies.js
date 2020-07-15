f7cc65c4777a977fdf616bc918a574c0888c1241
MariaJaci <mary.ja.ci@hotmail.com>
2020-07-15 13:42:58 +0100

    crud supply done

--- controllers/supplies.js
+++ controllers/supplies.js
@@ -16,16 +16,72 @@ exports.getSupplies = asyncHandler(async (req, res, next) => {
 // @route GET/api/v1/supplies/:id
 // @access Public
 exports.getSupply = asyncHandler(async (req, res, next) => {
-  const supply = await Supply.findById(req, res, next);
+  const supply = await Supply.findById(req.params.id);
 
   if (!supply) {
     return next(
       new ErrorResponse(`No supply found with the id of ${req.params.id}`),
-      400
+      404
     );
   }
   res.status(200).json({
     success: true,
     data: supply,
+  });
+});
+
+// @desc  Add new supply
+// @route POST /api/v1/supplies
+// @access Private
+exports.addSupply = asyncHandler(async (req, res, next) => {
+  //insert data into the database
+
+  const supply = await Supply.create(req.body);
+  // 201 response data created
+  res.status(201).json({
+    success: true,
+    data: supply,
+  });
+});
+
+// @desc  Update supply
+// @route PUT /api/v1/supplies/:id
+// @access Private
+exports.updateSupply = asyncHandler(async (req, res, next) => {
+  let supply = await Supply.findById(req.params.id);
+
+  if (!supply) {
+    return next(
+      new ErrorResponse(`No supply found with the id of ${req.params.id}`),
+      404
+    );
+  }
+  supply = await Supply.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: supply,
+  });
+});
+
+// @desc  Delete supply
+// @route DELETE /api/v1/supplies/:id
+// @access Private
+exports.deleteSupply = asyncHandler(async (req, res, next) => {
+  const supply = await Supply.findById(req.params.id);
+
+  if (!supply) {
+    return next(
+      new ErrorResponse(`No supply found with the id of ${req.params.id}`),
+      404
+    );
+  }
+  await supply.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
--- routes/supplies.js
+++ routes/supplies.js
@@ -1,10 +1,17 @@
 // Routes separate from the controllers just to make it cleaner possible
 const express = require('express');
-const { getSupplies, getSupply } = require('../controllers/supplies');
+const {
+  getSupplies,
+  getSupply,
+  addSupply,
+  updateSupply,
+  deleteSupply,
+} = require('../controllers/supplies');
 
 const router = express.Router();
 
-router.route('/').get(getSupplies);
-router.route('/:id').get(getSupply);
+// call controllers methods
+router.route('/').get(getSupplies).post(addSupply);
+router.route('/:id').get(getSupply).put(updateSupply).delete(deleteSupply);
 
 module.exports = router;
